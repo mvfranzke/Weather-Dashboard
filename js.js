99a71099a9f953393368d4b12a3c34861490f6b1
mvfranzke <michellevonfranzke@gmail.com>
2023-06-06 22:52:22 -0700

    declare element reference variable

---
+++ js.js
@@ -0,0 +1,9 @@
+let city;
+let queryURL = "https://api.openweathermap.org";
+let APIKey = "d7c0697d35036ba74a2e4280f0302ecc";
+let searchForm = document.getElementById("search-form");
+let searchInput = document.getElementById("search-input");
+let todayContainer = document.getElementById("today");
+let forecastContainer = document.getElementById("forecast");
+let cityContainer = document.getElementById("history");
+
